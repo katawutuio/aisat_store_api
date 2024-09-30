7e5fba6dcd6bef163f8c5d387345cd0dd43f6d7d
Katawut Keng <katawut.ut@yipintsoi.com>
2024-09-30 23:40:51 +0700

    create simple route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const helloRoute = require('./routes/hello');
+
 const PORT = 3000;
 
 const app = express();
+
+app.use(helloRoute);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`)
