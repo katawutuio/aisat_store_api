c7d641c0a97ec4e3ec22ee485a90ec5379ee6d29
Katawut Keng <katawut.ut@yipintsoi.com>
2024-10-01 01:28:20 +0700

    hash password ด้วย bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const authRouter = express.Router();
@@ -12,7 +13,11 @@ authRouter.post('/api/signup', async (req, res) => {
     if (existEmail) {
       return res.status(400).json({msg: "user email already exist"});
     } else {
-      var user = new User({fullName, email, password});
+      // generate a salt wit h a cost factor of 10
+      const salt = await bcrypt.genSalt(10);
+      // hash the password using the generated salt
+      const hashedPassword = await bcrypt.hash(password, salt);
+      var user = new User({fullName, email, password: hashedPassword});
       user = await user.save();
       return res.json({user});
     }
