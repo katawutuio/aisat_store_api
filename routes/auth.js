0146fa8ecbe8902b0e71fada162e27c19c3923c2
Katawut Keng <katawut.ut@yipintsoi.com>
2024-10-01 18:11:44 +0700

    create signin api endpoint - install jsonwebtoken ทำ token - ตรวจสอบ email, password - compare password ด้วย bcrypt - แยก sensitive information ออกกาอนจะ response user information กลับไป

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const authRouter = express.Router();
@@ -23,6 +24,36 @@ authRouter.post('/api/signup', async (req, res) => {
     }
   } catch (e) {
     return res.status(500).json({error: e.message});
+  }
+});
+
+authRouter.post('/api/signin', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const findUser = await User.findOne({email});
+
+    if(!findUser) {
+      return res.status(400).json({msg: "User not found with this email"});
+    } else {
+      // compare password
+      const isMatch = await bcrypt.compare(password, findUser.password);
+
+      if(!isMatch) {
+        return res.status(400).json({msg: "Incorrect Password"});
+      } else {
+        // if authen pass generate token for user
+        const token = jwt.sign({id:findUser._id}, "passwordkey")
+
+        // แยก password (sensitive information) ออกจาก user information
+        // ._doc คือ mongo document ในที่นี้คือ users 
+        const {password, ...userWithoutPassword} = findUser._doc;      
+
+        // send the response
+        res.json({token, ...userWithoutPassword});
+      }
+    }
+  } catch (error) {
+    
   }
 });
 
