77cdfdf72bade81f069d134a9a0d0341e8017695
Katawut Keng <katawutu.io@gmail.com>
2024-10-05 17:46:43 +0700

    create subcategory route

---
+++ routes/sub_category.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const SubCategory = require("../models/sub_category");
+const subCategoryRouter = express.Router();
+
+subCategoryRouter.post('/api/subcategories', async (req, res) => {
+  try {
+    const {categoryId, categoryName, image, subCategoryName} = req.body;
+    const subcategory = new SubCategory({categoryId, categoryName, image, subCategoryName});
+    await subcategory.save();
+    return res.status(201).send(subcategory);
+  } catch (error) {
+    return res.status(500).json({error: e.message}) ;
+  }
+});
+
+module.exports = subCategoryRouter;
